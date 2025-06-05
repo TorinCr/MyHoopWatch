1556c9d2a279841622016e44abe5c4f1cdf9f114
TorinCr <130181223+TorinCr@users.noreply.github.com>
2025-06-05 15:59:29 -0600

    Added Team images in routes, and json. Edited flex box for a more polished look. Refactored home.html for a more polished look

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,6 @@ from .services.kenpom_engine import get_kpoy
 import json
 from pathlib import Path
 
-
 routes = Blueprint('routes', __name__)
 
 @routes.route('/')
@@ -13,10 +12,12 @@ def home():
   players_df = get_kpoy(kenpom_browser)
   players_list = players_df.to_dict('records')
   
+  team_image_path = Path("app/static/data/team_images.json")
   image_path = Path("app/static/data/player_images.json")
   player_images = json.loads(image_path.read_text())
+  team_images = json.loads(team_image_path.read_text())
 
-  return render_template('home.html', players = players_list, image = player_images)
+  return render_template('home.html', players = players_list, image = player_images, team_logo = team_images)
 
 
 @routes.route('/nba')
